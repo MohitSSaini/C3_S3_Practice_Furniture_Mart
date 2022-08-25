323cda3c52363fac87cff15be07df1f016def440
Mohit Saini <mohit.saini09868@gmail.com>
2022-08-25 22:45:24 +0530

    Create a main method file in furnitureItemImpl

---
+++ src/main/java/com/furniture/jdp/furnitureItemImpl.java
@@ -0,0 +1,12 @@
+/*
+ * Author Name: Mohit Saini
+ * Date: 25-08-2022
+ * Created With: IntelliJ IDEA Community Edition
+ */
+package com.furniture.jdp;
+
+public class furnitureItemImpl {
+    public static void main ( String[] args ) {
+    
+    }
+}
