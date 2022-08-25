ee58adb5e7308ac2d4231a29a5b2ae0a59971f88
Mohit Saini <mohit.saini09868@gmail.com>
2022-08-26 00:33:38 +0530

    declare The main method which is used to run the program.

--- src/main/java/com/furniture/jdp/furnitureItemImpl.java
+++ src/main/java/com/furniture/jdp/furnitureItemImpl.java
@@ -6,7 +6,21 @@
 package com.furniture.jdp;
 
 public class furnitureItemImpl {
+    // The main method which is used to run the program.
     public static void main ( String[] args ) {
-    
+        furnitureItem input = new furnitureItem ( );
+        input.setFurnitureCode ( 66732 );
+        System.out.println ( "Furniture Code = " + input.getFurnitureCode ( ) );
+        input.setFurnitureType ( "Chair" );
+        System.out.println ( "Furniture Type = " + input.getFurnitureType ( ) );
+        input.setFurnitureGrade ( "Grade1" );
+        System.out.println ( "Furniture Grade = " + input.getFurnitureGrade ( ) );
+        input.setFurnitureColor ( "Red" );
+        System.out.println ( "Furniture Color = " + input.getFurnitureColor ( ) );
+        input.setFurnitureUsage ( "OutDoor" );
+        System.out.println ( "FurnitureUsage = " + input.getFurnitureUsage ( ) );
+        input.setFurniturePrice ( 1500.00f );
+        System.out.println ( "Furniture Price = " + input.getFurniturePrice ( ) );
+        System.out.println ( "Discount Price is = " + input.calculateDiscount ( ) );
     }
 }
